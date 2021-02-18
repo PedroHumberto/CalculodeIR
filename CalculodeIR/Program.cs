a96de3396ced559691d0d0dca7762a94f19869d0
PedroHumberto <78572029+PedroHumberto@users.noreply.github.com>
2021-02-18 01:08:43 -0300

    Update Program.cs
    Correções nas variáveis e valores que estavam diferentes da tabela 2020

--- CalculodeIR/Program.cs
+++ CalculodeIR/Program.cs
@@ -27,36 +27,36 @@ namespace CalculodeIR
             baseDeCalculo = aluguel - comissao;
 
             //Caluclo do imposto
-            var imposto;
+            double imposto = 0;
 
-            if (baseDeCalculo > 4664.67)
+            if (baseDeCalculo >= 4664.68)
             {
                 imposto = (0.275 * baseDeCalculo) - 869.36;
             }
 
-            else if (baseDeCalculo > 3751.04 && baseDeCalculo < 4664.68)
+            else if (baseDeCalculo >= 3751.06 && baseDeCalculo <= 4664.68)
             {
                 imposto = (0.225 * baseDeCalculo) - 636.13;
             }
 
-            else if (baseDeCalculo > 2826.65 && baseDeCalculo < 3751.05)
+            else if (baseDeCalculo >= 2826.66 && baseDeCalculo <= 3751.05)
             {
                 imposto = (0.150 * baseDeCalculo) - 354.80;
             }
 
-            else if (baseDeCalculo > 1903.98 && baseDeCalculo < 2826.65)
+            else if (baseDeCalculo >= 1903.99 && baseDeCalculo <= 2826.65)
             {
                 imposto = (0.075 * baseDeCalculo) - 142.80;
             }
 
-            else if (baseDeCalculo < 1903.98)
+            else if (baseDeCalculo <= 1903.98)
             {
                 Console.WriteLine("Não tem IR");
+              
             }
-            else
-            {
-                Console.WriteLine("O Valor do impsto de renda é R$" + imposto)
-            }
+
+                Console.WriteLine("O Valor do impsto de renda é R$" + imposto.ToString("F2"));
+            
             
             //Para dar o retorno apos preencher qualquer coisa.
             Console.ReadLine();
